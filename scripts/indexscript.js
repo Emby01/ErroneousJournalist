fbc6540d1f0a2ada6d31a05ea1b8bb4f3334dfc0
Emby <99022462+Emby01@users.noreply.github.com>
2023-02-10 22:40:21 +1100

    Change carousel refresh to 3 seconds
    Also changed the other titles that have nothing to say [insert title here]

--- scripts/indexscript.js
+++ scripts/indexscript.js
@@ -26,5 +26,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
